014d637604db53e6e1532dad400a343f62524b61
Kushal Rathod <kushalrathod044@gmail.com>
2025-12-22 19:11:04 +0530

    add: Experience
    Added experience regarding bijoi

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -24,7 +24,7 @@ const Experience = () => {
           <div className="flex justify-between items-end">
             <div className="flex items-end gap-3">
               <Link className="inline-block" href="https://www.clubduelz.in/" target="_blank">
-                <Image src="/companies/clubduelz.jpg" alt="Club Duelz" width={1000} height={1000} priority draggable={false} className="rounded-lg h-15 w-15 border border-dashed hover:border-primary" />
+                <Image src="/companies/clubduelz.jpg" alt="Club Duelz" width={1000} height={1000} priority draggable={false} className="rounded-lg h-15 w-15 p-1 border border-dashed hover:border-primary" />
               </Link>
               <div className="flex flex-col">
                 <h1 className="text-lg font-bold">Club Duelz</h1>
@@ -63,7 +63,51 @@ const Experience = () => {
             <li>Developed multiple new service layers using Axios with modular interceptors, improving API reliability, error handling, and scalability.</li>
           </div>
         </div>
+        <div className="h-px w-full bg-gray-500/30 my-5"></div>
         {/* Bijoi */}
+        <div className="flex flex-col gap-3 mt-5 border border-dashed p-3 rounded-lg">
+          <div className="flex justify-between items-end">
+            <div className="flex items-end gap-3">
+              <Link className="inline-block" href="https://www.bijoi.in/" target="_blank">
+                <Image src="/companies/bijoi.avif" alt="Bijoi" width={1000} height={1000} priority draggable={false} className="rounded-lg h-15 w-15 border border-dashed p-1 hover:border-primary" />
+              </Link>
+              <div className="flex flex-col">
+                <h1 className="text-lg font-bold">Bijoi</h1>
+                <p className="text-sm -mt-1 text-gray-500">Web Dev Intern</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-end text-gray-500">
+              <h1>May 2025 - June 2025</h1>
+              <h1 className="-mt-1">India - Onsite</h1>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-1">
+              <Link href="https://www.bijoi.in/" target="_blank">
+                <CircleArrowOutUpRight className="h-5 w-5 opacity-50" />
+              </Link>
+              <Link href="https://www.instagram.com/_bijoi_/" target="_blank">
+                <Instagram className="h-6 w-6 opacity-50" />
+              </Link>
+            </div>
+            <div className="border rounded-lg bg-green-100 border-green-500 inner-shadow flex items-center gap-2 px-4 py-1">
+              <div className="h-3 w-3 rounded-full bg-green-500"></div>
+              <p className="text-sm text-black">Working</p>
+            </div>
+          </div>
+          {/* <div>
+            <h1 className="text-lg font-bold">Tools & Technologies used</h1>
+            <div className="flex flex-wrap gap-y-2 mt-3">
+              <LanguageBox data={used_tech} />
+            </div>
+          </div>
+          <div className="*:mt-1 mt-2 px-5 text-lg text-gray-500">
+            <li>Refactored all existing data-retrieval flows to use TanStack Query, significantly improving request deduplication, caching efficiency, and client-side performance.</li>
+            <li>Built a BookMyShow-style dynamic interest-recommendation module, enabling personalized content surfacing based on real-time user preferences.</li>
+            <li>Added a secure user profile creation workflow, including automated NSFW content detection to ensure platform integrity and compliance.</li>
+            <li>Developed multiple new service layers using Axios with modular interceptors, improving API reliability, error handling, and scalability.</li>
+          </div> */}
+        </div>
     </div>
   )
 }
